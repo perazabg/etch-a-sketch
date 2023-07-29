b12002030b04d59bbe40dae2ee62d626d713cf00
perazabg <96875802+perazabg@users.noreply.github.com>
2023-07-28 23:33:46 -0700

    completed for now

--- main.js
+++ main.js
@@ -1,29 +1,66 @@
-const defaultSize = 16;
-
+const DEFAULT_SIZE = 16;
 const grid = document.getElementById('gridContainer');
+const defaultColor = document.getElementById('resetBtn');
+const rgbBtn = document.getElementById('rgbBtn');
+const slider = document.getElementById('slider');
+const DEFAULT_COLOR = 'background-color: black';
+const sliderValue = document.getElementById('sliderValue');
 
-function createGrid(defaultSize) {
+
+slider.addEventListener('change', event => {
+  /*grid.innerHTML = '';*/
+  let newSize = slider.value;
+  sliderValue.innerHTML = `${newSize}`
+})
+
+defaultColor.addEventListener('click', event => {
+  let newSize = slider.value;
+    createGrid(newSize);
+})
+
+rgbBtn.addEventListener('click', event => {
+  let newSize = slider.value;
+  rgbGrid(newSize); //fix here
+  
+})
+
+
+
+function createGrid(newSize) {
   grid.innerHTML = ''; // Clear the existing grid (if any)
-
+  
   grid.style.display = 'grid'; // Set the grid layout
-  grid.style.gridTemplateColumns = `repeat(${defaultSize}, auto)`; 
-  grid.style.gridTemplateRows = `repeat(${defaultSize}, auto)`;
-  for (let x = 0; x < defaultSize * defaultSize; x++) {
+  grid.style.gridTemplateColumns = `repeat(${newSize}, auto)`; 
+  grid.style.gridTemplateRows = `repeat(${newSize}, auto)`;
+  for (let x = 0; x < newSize * newSize; x++) {
     let cell = document.createElement('div');
     cell.classList.add('cell');
     grid.appendChild(cell);
     cell.addEventListener('pointerover', e => {
-      cell.style = 'background-color: black';
-      
+      cell.style = DEFAULT_COLOR;
     })
   }
 }
 
-function changeColor(){
+function rgbGrid(newSize){
+  grid.innerHTML = ''; // Clear the existing grid (if any)
   
+  grid.style.display = 'grid'; // Set the grid layout
+  grid.style.gridTemplateColumns = `repeat(${newSize}, auto)`; 
+  grid.style.gridTemplateRows = `repeat(${newSize}, auto)`;
+  for (let x = 0; x < newSize * newSize; x++) {
+    let cell = document.createElement('div');
+    cell.classList.add('cell');
+    grid.appendChild(cell);
+    cell.addEventListener('pointerover', e => {
+      let randomColor = "#" + Math.floor(Math.random()*16777215).toString(16);
+      cell.style = "background-color: " + randomColor;
+    })
+  }
 }
 
-const btn = document.getElementById('resetBtn');
-btn.addEventListener('click', event => {
-    createGrid(defaultSize);
+window.addEventListener("load", event => {
+  let size = slider.value;
+  sliderValue.innerHTML = size;
+  createGrid(DEFAULT_SIZE);
 });
